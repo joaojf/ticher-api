960429f1704288e94c17c41431108f3cdfbdb1b6
gtanques <saintcalm1@gmail.com>
2021-11-15 00:07:50 -0300

    adiciona data de criação a disponibilidade do professor

--- src/main/java/com/projetofinal/ticher/models/TeacherAvailability.java
+++ src/main/java/com/projetofinal/ticher/models/TeacherAvailability.java
@@ -3,6 +3,7 @@ package com.projetofinal.ticher.models;
 import com.projetofinal.ticher.models.abstracts.SchedulingClass;
 
 import javax.persistence.*;
+import java.time.Instant;
 
 @Entity
 public class TeacherAvailability extends SchedulingClass {
@@ -13,6 +14,9 @@ public class TeacherAvailability extends SchedulingClass {
 
     @OneToOne
     private Teacher teachers;
+
+    @Column(nullable = false, updatable = false)
+    private final Instant createdIn = Instant.now();
 
     @Deprecated
     private TeacherAvailability() {
